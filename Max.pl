beb334250cdab1027aa3502ea78c53a51ecca87e
mehul jain <75320785+mehuljain133@users.noreply.github.com>
2025-04-30 15:43:50 +0530

    Delete Max.pl

--- Max.pl
+++
@@ -1,9 +0,0 @@
-% Write a Prolog program to implement max(X, Y, M) so that M is the maximum of two numbers X and Y
-
-% --------------------------------------------
-% max(X, Y, M) :-
-%     M is the maximum of the two numbers X and Y.
-% --------------------------------------------
-
-max(X, Y, X) :- X >= Y.
-max(X, Y, Y) :- Y > X.
